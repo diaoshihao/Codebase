d9ccfd313f017d1ef7e505f2180cd826db9d7dd2
suger <suger@sugerdeiMac.local>
2017-10-27 18:38:39 +0800

    create codebase

---
+++ Codebase/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Codebase
+//
+//  Created by suger on 2017/10/27.
+//  Copyright © 2017年 diaoshihao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
